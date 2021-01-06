53fe81dd76450dd483e33f642b921520ec50e044
DestroyedClone <DestroyedClone@gmail.com>
2021-01-05 20:29:38 -0800

    Add project files.

---
+++ ROR2-TF2Survivors/MainPlugin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using BepInEx;
+using R2API;
+using R2API.Utils;
+using EntityStates;
+using RoR2;
+using RoR2.Skills;
+using UnityEngine;
+using UnityEngine.Networking;
+using KinematicCharacterController;
+
+namespace ROR2_TF2Survivors
+{
+    [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
+    [BepInPlugin(ModGuid, ModName, ModVer)]
+
+    public class MainPlugin : BaseUnityPlugin
+    {
+        public const string ModVer = "1.0.0";
+        public const string ModName = "Risk of Bulletstorm";
+        public const string ModGuid = "com.DestroyedClone.RiskOfBulletstorm";
+
+
+    }
+}
