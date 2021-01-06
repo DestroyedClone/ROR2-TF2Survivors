ca075ca5a2ea19b3d437cac9b2b3e47446b9df43
DestroyedClone <DestroyedClone@gmail.com>
2021-01-05 21:59:10 -0800

    Set up skeleton and filled the tokens

--- ROR2-TF2Survivors/MainPlugin.cs
+++ ROR2-TF2Survivors/MainPlugin.cs
@@ -15,13 +15,70 @@ namespace ROR2_TF2Survivors
     [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
     [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
     [BepInPlugin(ModGuid, ModName, ModVer)]
+    [R2APISubmoduleDependency(nameof(PrefabAPI), nameof(SurvivorAPI), nameof(LoadoutAPI), nameof(ItemAPI), nameof(DifficultyAPI), nameof(BuffAPI))]
 
-    public class MainPlugin : BaseUnityPlugin
+    public class TF2Survivors : BaseUnityPlugin
     {
         public const string ModVer = "1.0.0";
-        public const string ModName = "Risk of Bulletstorm";
-        public const string ModGuid = "com.DestroyedClone.RiskOfBulletstorm";
+        public const string ModName = "Team Fortress 2 Survivors";
+        public const string ModGuid = "com.DestroyedClone.RORTF2Survivors";
 
+        public static TF2Survivors instance;
+
+        public static event Action awake;
+        public static event Action start;
+
+        public TF2Survivors()
+        {
+            awake += TF2Survivors_Load;
+            start += TF2Survivors_LoadStart;
+        }
+
+        private void TF2Survivors_Load()
+        {
+            instance = this;
+
+            Modules.Assets.PopulateAssets();
+            Modules.Config.ReadConfig();
+
+            CreatePrefab();
+            CreateDisplayPrefab();
+            RegisterCharacter();
+            Modules.Skins.RegisterSkins();
+            Modules.Buffs.RegisterBuffs();
+            Modules.Projectiles.RegisterProjectiles();
+            Modules.ItemDisplays.RegisterDisplays();
+            Modules.Effects.RegisterEffects();
+            Modules.Unlockables.RegisterUnlockables();
+            Modules.Tokens.AddTokens();
+
+            CreateDoppelganger();
+
+            Hook();
+        }
+
+        private void TF2Survivors_LoadStart()
+        {
+        }
+
+        public void Awake()
+        {
+            Action awake = TF2Survivors.awake;
+            if (awake == null)
+            {
+                return;
+            }
+            awake();
+        }
+        public void Start()
+        {
+            Action start = TF2Survivors.start;
+            if (start == null)
+            {
+                return;
+            }
+            start();
+        }
 
     }
 }
---
+++ ROR2-TF2Survivors/SaxtonHale/SaxtonHale.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using BepInEx;
+using R2API;
+using R2API.Utils;
+using EntityStates;
+using RoR2;
+using RoR2.Skills;
+using UnityEngine;
+using UnityEngine.Networking;
+using KinematicCharacterController;
+
+namespace ROR2_TF2Survivors
+{
+    public class SaxtonHale
+    {
+        public static TF2Survivors instance;
+
+        public static GameObject characterPrefab;
+
+        public GameObject doppelganger;
+
+        public static event Action Awake;
+        public static event Action Start;
+
+        public static readonly Color characterColor = Color.yellow;
+
+        public SkillLocator skillLocator;
+        public void Init()
+        {
+            CreatePrefab();
+            //CreateDisplayPrefab();
+            RegisterCharacter();
+            NemItemDisplays.RegisterDisplays();
+            NemforcerSkins.RegisterSkins();
+            RegisterProjectiles();
+            CreateDoppelganger();
+            CreateBossPrefab();
+        }
+
+        private static void CreatePrefab()
+        {
+            characterPrefab = PrefabAPI.InstantiateClone(Resources.Load<GameObject>("Prefabs/CharacterBodies/LoaderBody"), "TF2SaxtonHale");
+
+            characterPrefab.GetComponent<NetworkIdentity>().localPlayerAuthority = true;
+
+            //GameObject model = CreateModel(characterPrefab, 0);
+
+            CharacterDirection characterDirection = characterPrefab.GetComponent<CharacterDirection>();
+            characterDirection.moveVector = Vector3.zero;
+            //characterDirection.targetTransform = gameObject.transform;
+            characterDirection.overrideAnimatorForwardTransform = null;
+            characterDirection.rootMotionAccumulator = null;
+            //characterDirection.modelAnimator = model.GetComponentInChildren<Animator>();
+            characterDirection.driveFromRootRotation = false;
+            characterDirection.turnSpeed = 720f;
+
+            CharacterBody bodyComponent = characterPrefab.GetComponent<CharacterBody>();
+            bodyComponent.bodyIndex = -1;
+            bodyComponent.name = "TF2SaxtonHale";
+            bodyComponent.baseNameToken = "SAXTONHALE_NAME";
+            bodyComponent.subtitleNameToken = "SAXTONHALE_SUBTITLE";
+            bodyComponent.bodyFlags = CharacterBody.BodyFlags.IgnoreFallDamage | CharacterBody.BodyFlags.ImmuneToExecutes;
+            bodyComponent.rootMotionInMainState = false;
+            bodyComponent.mainRootSpeed = 0;
+            bodyComponent.baseMaxHealth = 200;
+            bodyComponent.levelMaxHealth = 20;
+            bodyComponent.baseRegen = 1f;
+            bodyComponent.levelRegen = 0.5f;
+            bodyComponent.baseMaxShield = 0;
+            bodyComponent.levelMaxShield = 0;
+            bodyComponent.baseMoveSpeed = 7;
+            bodyComponent.levelMoveSpeed = 0;
+            bodyComponent.baseAcceleration = 80;
+            bodyComponent.baseJumpPower = 15;
+            bodyComponent.levelJumpPower = 0;
+            bodyComponent.baseDamage = 12;
+            bodyComponent.levelDamage = 2.4f;
+            bodyComponent.baseAttackSpeed = 1;
+            bodyComponent.levelAttackSpeed = 0;
+            bodyComponent.baseCrit = 1;
+            bodyComponent.levelCrit = 0;
+            bodyComponent.baseArmor = 15;
+            bodyComponent.levelArmor = 0f;
+            bodyComponent.baseJumpCount = 1;
+            bodyComponent.sprintingSpeedMultiplier = 1f; //1.45
+            bodyComponent.wasLucky = false;
+            bodyComponent.hideCrosshair = false;
+            //bodyComponent.crosshairPrefab = Resources.Load<GameObject>("Prefabs/Crosshair/SimpleDotCrosshair");
+            //bodyComponent.aimOriginTransform = gameObject3.transform;
+            bodyComponent.hullClassification = HullClassification.Human;
+            //bodyComponent.portraitIcon = Modules.Assets.charPortrait;
+            bodyComponent.isChampion = false;
+            bodyComponent.currentVehicle = null;
+            bodyComponent.skinIndex = 0U;
+            bodyComponent.preferredPodPrefab = null;
+            /*
+            LoadoutAPI.AddSkill(typeof(States.PaladinMain));
+            LoadoutAPI.AddSkill(typeof(States.SpawnState));
+            LoadoutAPI.AddSkill(typeof(States.Emotes.BaseEmote));
+            LoadoutAPI.AddSkill(typeof(States.Emotes.PraiseTheSun));
+            LoadoutAPI.AddSkill(typeof(States.Emotes.PointDown));
+            LoadoutAPI.AddSkill(typeof(States.Emotes.Rest));
+            */
+
+            var stateMachine = bodyComponent.GetComponent<EntityStateMachine>();
+            stateMachine.mainStateType = new SerializableEntityStateType(typeof(States.PaladinMain));
+            stateMachine.initialStateType = new SerializableEntityStateType(typeof(States.SpawnState));
+
+            CharacterMotor characterMotor = characterPrefab.GetComponent<CharacterMotor>();
+            characterMotor.walkSpeedPenaltyCoefficient = 1f;
+            characterMotor.characterDirection = characterDirection;
+            characterMotor.muteWalkMotion = false;
+            characterMotor.mass = 100f;
+            characterMotor.airControl = 0.25f;
+            characterMotor.disableAirControlUntilCollision = false;
+            characterMotor.generateParametersOnAwake = true;
+
+            CameraTargetParams cameraTargetParams = characterPrefab.GetComponent<CameraTargetParams>();
+            cameraTargetParams.cameraParams = Resources.Load<GameObject>("Prefabs/CharacterBodies/MercBody").GetComponent<CameraTargetParams>().cameraParams;
+            cameraTargetParams.cameraPivotTransform = null;
+            cameraTargetParams.aimMode = CameraTargetParams.AimType.Standard;
+            cameraTargetParams.recoil = Vector2.zero;
+            cameraTargetParams.idealLocalCameraPos = Vector3.zero;
+            cameraTargetParams.dontRaycastToPivot = false;
+
+            //ChildLocator childLocator = model.GetComponent<ChildLocator>();
+
+            TeamComponent teamComponent = null;
+            if (characterPrefab.GetComponent<TeamComponent>() != null) teamComponent = characterPrefab.GetComponent<TeamComponent>();
+            else teamComponent = characterPrefab.GetComponent<TeamComponent>();
+            teamComponent.hideAllyCardDisplay = false;
+            teamComponent.teamIndex = TeamIndex.None;
+
+            HealthComponent healthComponent = characterPrefab.GetComponent<HealthComponent>();
+            healthComponent.shield = 0f;
+            healthComponent.barrier = 0f;
+            healthComponent.magnetiCharge = 0f;
+            healthComponent.body = null;
+            healthComponent.dontShowHealthbar = false;
+            healthComponent.globalDeathEventChanceCoefficient = 1f;
+
+            characterPrefab.GetComponent<Interactor>().maxInteractionDistance = 3f;
+            characterPrefab.GetComponent<InteractionDriver>().highlightInteractor = true;
+
+            CharacterDeathBehavior characterDeathBehavior = characterPrefab.GetComponent<CharacterDeathBehavior>();
+            characterDeathBehavior.deathStateMachine = characterPrefab.GetComponent<EntityStateMachine>();
+            //characterDeathBehavior.deathState = new SerializableEntityStateType(typeof(GenericCharacterDeath));
+
+            SfxLocator sfxLocator = characterPrefab.GetComponent<SfxLocator>();
+            //sfxLocator.deathSound = Sounds.DeathSound;
+            sfxLocator.barkSound = "";
+            sfxLocator.openSound = "";
+            sfxLocator.landingSound = "Play_char_land";
+            sfxLocator.fallDamageSound = "Play_char_land_fall_damage";
+            sfxLocator.aliveLoopStart = "";
+            sfxLocator.aliveLoopStop = "";
+
+            Rigidbody rigidbody = characterPrefab.GetComponent<Rigidbody>();
+            rigidbody.mass = 100f;
+            rigidbody.drag = 0f;
+            rigidbody.angularDrag = 0f;
+            rigidbody.useGravity = false;
+            rigidbody.isKinematic = true;
+            rigidbody.interpolation = RigidbodyInterpolation.None;
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Discrete;
+            rigidbody.constraints = RigidbodyConstraints.None;
+
+            CapsuleCollider capsuleCollider = characterPrefab.GetComponent<CapsuleCollider>();
+            capsuleCollider.isTrigger = false;
+            capsuleCollider.material = null;
+            capsuleCollider.center = new Vector3(0f, 0f, 0f);
+            //capsuleCollider.radius = 0.5f;
+            //capsuleCollider.height = 1.82f;
+            //capsuleCollider.direction = 1;
+
+            KinematicCharacterMotor kinematicCharacterMotor = characterPrefab.GetComponent<KinematicCharacterMotor>();
+            kinematicCharacterMotor.CharacterController = characterMotor;
+            kinematicCharacterMotor.Capsule = capsuleCollider;
+            kinematicCharacterMotor.Rigidbody = rigidbody;
+            kinematicCharacterMotor.DetectDiscreteCollisions = false;
+            kinematicCharacterMotor.GroundDetectionExtraDistance = 0f;
+            kinematicCharacterMotor.MaxStepHeight = 0.2f;
+            kinematicCharacterMotor.MinRequiredStepDepth = 0.1f;
+            kinematicCharacterMotor.MaxStableSlopeAngle = 55f;
+            kinematicCharacterMotor.MaxStableDistanceFromLedge = 0.5f;
+            kinematicCharacterMotor.PreventSnappingOnLedges = false;
+            kinematicCharacterMotor.MaxStableDenivelationAngle = 55f;
+            kinematicCharacterMotor.RigidbodyInteractionType = RigidbodyInteractionType.None;
+            kinematicCharacterMotor.PreserveAttachedRigidbodyMomentum = true;
+            kinematicCharacterMotor.HasPlanarConstraint = false;
+            kinematicCharacterMotor.PlanarConstraintAxis = Vector3.up;
+            kinematicCharacterMotor.StepHandling = StepHandlingMethod.None;
+            kinematicCharacterMotor.LedgeHandling = true;
+            kinematicCharacterMotor.InteractiveRigidbodyHandling = true;
+            kinematicCharacterMotor.SafeMovement = false;
+
+            /*
+            FootstepHandler footstepHandler = model.AddComponent<FootstepHandler>();
+            footstepHandler.baseFootstepString = "Play_player_footstep";
+            footstepHandler.sprintFootstepOverrideString = "";
+            footstepHandler.enableFootstepDust = true;
+            footstepHandler.footstepDustPrefab = Resources.Load<GameObject>("Prefabs/GenericFootstepDust");
+            */
+
+            PhysicMaterial physicMat = Resources.Load<GameObject>("Prefabs/CharacterBodies/CommandoBody").GetComponentInChildren<RagdollController>().bones[1].GetComponent<Collider>().material;
+
+            characterPrefab.AddComponent<Misc.PaladinSwordController>();
+        }
+
+        private void RegisterCharacter()
+        {
+            //characterDisplay.AddComponent<NetworkIdentity>();
+
+            //string unlockString = "PALADIN_UNLOCKABLE_REWARD_ID";
+            //if (Modules.Config.forceUnlock.Value) unlockString = "";
+
+            SurvivorDef survivorDef = new SurvivorDef
+            {
+                name = "SAXTONHALE_NAME",
+                //unlockableName = unlockString,
+                descriptionToken = "SAXTONHALE_DESCRIPTION",
+                primaryColor = characterColor,
+                bodyPrefab = characterPrefab,
+                displayPrefab = characterDisplay,
+                outroFlavorToken = "SAXTONHALE_OUTRO_FLAVOR"
+            };
+
+            SurvivorAPI.AddSurvivor(survivorDef);
+
+            SkillSetup();
+
+            BodyCatalog.getAdditionalEntries += delegate (List<GameObject> list)
+            {
+                list.Add(characterPrefab);
+            };
+
+            characterPrefab.tag = "Player";
+        }
+    }
+}
