9729f2f6c833c1cf5ebd3510a8ed40bf5e0c2806
DestroyedClone <DestroyedClone@gmail.com>
2021-01-08 00:37:55 -0800

    added equipments

---
+++ ROR2-TF2Survivors/Scout/Equipment/equipmentboilerplate.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx.Configuration;
+using R2API;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ROR2_TF2Survivors.Base_Classes;
+
+namespace ROR2_TF2Survivors.Scout
+{
+    public class BonkEquipment : EquipmentBase
+    {
+        public override string EquipmentName => throw new NotImplementedException();
+        public override string EquipmentLangTokenName => throw new NotImplementedException();
+        public override string EquipmentPickupDesc => throw new NotImplementedException();
+        public override string EquipmentFullDescription => throw new NotImplementedException();
+        public override string EquipmentLore => throw new NotImplementedException();
+        public override string EquipmentModelPath => throw new NotImplementedException();
+        public override string EquipmentIconPath => throw new NotImplementedException();
+
+        public override void Init(ConfigFile config)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ItemDisplayRuleDict CreateItemDisplayRules()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Hooks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
