22e5817047185978f7ba43972f65fceb7ea35d08
DestroyedClone <DestroyedClone@gmail.com>
2021-01-09 01:24:51 -0800

    Added pistol skill state

---
+++ ROR2-TF2Survivors/States/FireScoutPistol.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EntityStates;
+
+namespace ROR2_Scout.States
+{
+    public class FireScoutPistol : EntityStates.Commando.CommandoWeapon.FirePistol
+    {
+
+    }
+}
