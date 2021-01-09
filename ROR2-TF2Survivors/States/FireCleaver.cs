f386f39addd9071dfbb8554ab8035c18dd053965
DestroyedClone <DestroyedClone@gmail.com>
2021-01-09 01:45:49 -0800

    throwables

--- ROR2-TF2Survivors/States/FireCleaver.cs
+++ ROR2-TF2Survivors/States/FireCleaver.cs
@@ -2,10 +2,39 @@
 using System.Collections.Generic;
 using System.Text;
 using EntityStates;
+using UnityEngine;
+using RoR2.Projectile;
+using RoR2;
 
 namespace ROR2_Scout.States
 {
-    public class FireCleaver
+    public class FireCleaver : BaseSkillState
     {
-    }
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            this.duration = baseDuration / this.attackSpeedStat;
+            Ray aimRay = base.GetAimRay();
+            base.StartAimMode(aimRay, 2f, false);
+            base.PlayAnimation("Gesture", "FireFMJ", "FireFMJ.playbackRate", this.duration);
+            if (base.isAuthority)
+            {
+                ProjectileManager.instance.FireProjectile(projectilePrefab, aimRay.origin, Util.QuaternionSafeLookRotation(aimRay.direction), base.gameObject, this.damageStat * damageCoefficient, 0f, Util.CheckRoll(this.critStat, base.characterBody.master), DamageColorIndex.Bleed, null, -1f);
+            }
+        }
+
+        public override InterruptPriority GetMinimumInterruptPriority()
+        {
+            return InterruptPriority.PrioritySkill;
+        }
+
+		public static GameObject effectPrefab;
+        public static GameObject projectilePrefab = ROR2_Scout.Projectiles.cleaverProjectile;
+		public static float damageCoefficient;
+		public static float force;
+		public static float selfForce;
+		public static float baseDuration = 2f;
+		private float duration;
+		public int bulletCountCurrent = 1;
+	}
 }
---
+++ ROR2-TF2Survivors/States/FireMilk.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EntityStates;
+using UnityEngine;
+using RoR2.Projectile;
+using RoR2;
+
+namespace ROR2_Scout.States
+{
+    public class FireMilk : FireCleaver
+    {
+		public FireMilk()
+        {
+			damageCoefficient = 0f;
+			projectilePrefab = ROR2_Scout.Projectiles.milkjarProjectile;
+		}
+	}
+}
